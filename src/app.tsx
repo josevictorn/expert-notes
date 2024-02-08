559770e1a86bbd1954a6df491926f29c4809eb3e
victor <josevictornascimento2016@gmail.com>
2024-02-08 14:18:26 -0300

    feat: create note deletion feature

--- src/app.tsx
+++ src/app.tsx
@@ -34,15 +34,25 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  const filteredNotes = search !== ''
+  ? notes.filter(note => note.content.toLocaleLowerCase(). includes(search.toLocaleLowerCase()))
+  : notes
+
   function handleSearch(event: ChangeEvent<HTMLInputElement>) {
     const query = event.target.value
 
     setSearch(query)
   }
-
-  const filteredNotes = search !== ''
-    ? notes.filter(note => note.content.toLocaleLowerCase(). includes(search.toLocaleLowerCase()))
-    : notes
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
@@ -62,7 +72,7 @@ export function App() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
         
-        {filteredNotes.map(note => (<NoteCard note={note} />))}
+        {filteredNotes.map(note => (<NoteCard note={note} onNoteDeleted={onNoteDeleted} />))}
         
       </div>
     </div>
